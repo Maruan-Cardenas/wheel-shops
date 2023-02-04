90c5e999ea9b1e13e7ef1c15bcb59dd1606ea0c7
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2023-02-04 12:53:06 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const key = process.env.NEXT_PUBLIC_ANALYTICS_ID
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${key}`}
+      />
+      <Script strategy='lazyOnload' id='my-script'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${key}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
